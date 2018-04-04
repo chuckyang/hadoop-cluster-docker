4e0cebcca307e34a4a6523ed560eeb3aff4dcac2
chuckyang <yzhang.baby@gmail.com>
2018-04-04 15:54:23 +0800

    update

--- start-container.sh
+++ start-container.sh
@@ -9,8 +9,10 @@ sudo docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
 sudo docker run -itd \
                 --net=hadoop \
-                -p 50070:50070 \
+                -p 5007:50070 \
                 -p 8088:8088 \
+		-p 8030-8033:8030-8033 \
+		-p 29000:9000 \
                 --name hadoop-master \
                 --hostname hadoop-master \
                 jybaby1027/hadoop:v1 &> /dev/null

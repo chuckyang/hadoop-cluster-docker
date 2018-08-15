defa9eafd24eea9ba09dc32fd4eed3690a29191b
yzhang <55456943@qq.com>
2018-08-15 16:28:20 +0800

    update compose file

--- start-container.sh
+++ start-container.sh
@@ -5,16 +5,16 @@ N=${1:-3}
 
 
 # start hadoop master container
-sudo docker rm -f hadoop-master &> /dev/null
-echo "start hadoop-master container..."
+sudo docker rm -f hadoop_master &> /dev/null
+echo "start hadoop_master container..."
 sudo docker run -itd \
                 --net=hadoop \
                 -p 50070:50070 \
                 -p 8088:8088 \
 		-p 8030-8033:8030-8033 \
 		-p 29000:9000 \
-                --name hadoop-master \
-                --hostname hadoop-master \
+                --name hadoop_master \
+                --hostname hadoop_master \
                 jybaby1027/hadoop:v1 &> /dev/null
 
 
@@ -22,15 +22,15 @@ sudo docker run -itd \
 i=1
 while [ $i -lt $N ]
 do
-	sudo docker rm -f hadoop-slave$i &> /dev/null
-	echo "start hadoop-slave$i container..."
+	sudo docker rm -f hadoop_slave$i &> /dev/null
+	echo "start hadoop_slave$i container..."
 	sudo docker run -itd \
 	                --net=hadoop \
-	                --name hadoop-slave$i \
-	                --hostname hadoop-slave$i \
+	                --name hadoop_slave$i \
+	                --hostname hadoop_slave$i \
 	                jybaby1027/hadoop:v1 &> /dev/null
 	i=$(( $i + 1 ))
 done 
 
 # get into hadoop master container
-sudo docker exec -it hadoop-master bash
+sudo docker exec -it hadoop_master bash

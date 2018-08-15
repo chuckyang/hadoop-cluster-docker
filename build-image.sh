783e207edabf5f9054471ddb2e78756a5aeaf854
yzhang <55456943@qq.com>
2018-08-15 17:06:08 +0800

    update compose file

--- build-image.sh
+++ build-image.sh
@@ -3,6 +3,6 @@
 echo ""
 
 echo -e "\nbuild docker hadoop image\n"
-sudo docker build -t jybaby1027/hadoop:v1 .
+sudo docker build -t registry-vpc.cn-hangzhou.aliyuncs.com/yywh/hadoop:1.9.1 .
 
 echo ""
